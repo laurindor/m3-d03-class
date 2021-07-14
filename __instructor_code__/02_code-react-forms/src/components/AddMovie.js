f8f370e088a8be94fbaafda99741ac8f2cf18318
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-07-14 17:55:51 +0200

    WIP step 1 control one field

--- __instructor_code__/02_code-react-forms/src/components/AddMovie.js
+++ __instructor_code__/02_code-react-forms/src/components/AddMovie.js
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function AddMovie(props) {
   
+  const [movieTitle, setMovieTitle] = useState('Hello Pizza Bytes')
+
+
+  function handleTitleChange(event){
+    setMovieTitle(event.target.value)
+    console.log(event.target.value)
+  }
+
+  function handleOnSubmit(event){
+    event.preventDefault();
+    const newMovie = {title: movieTitle}
+    console.log(newMovie)
+    props.addMovieHandler(newMovie)
+  }
 
   return (
     <div className="AddMovie">
       <h4>Add a Movie</h4>
-      <form >
+      <form onSubmit={handleOnSubmit} >
         <label>Title:</label>
-        <input type="text" name="title" />
+        <input type="text" name="title" value={movieTitle} onChange={handleTitleChange} />
 
         <label>Director:</label>
         <input type="text" name="director" />
--- __instructor_code__/02_code-react-forms/src/components/MovieList.js
+++ __instructor_code__/02_code-react-forms/src/components/MovieList.js
@@ -6,8 +6,17 @@ import AddMovie from "./AddMovie";
 import FilterMovies from "./FilterMovies";
 
 function MovieList() {
+
   const [moviesData, setMoviesData] = useState(moviesDataJSON);
   const [movies, setMovies] = useState(moviesDataJSON);
+
+
+  function addMovie(newMovie){
+    const updatedMovieData = [ newMovie, ...moviesData ]
+    {/* this is equivalent to [newMovie].concat(moviesData) */}
+    setMoviesData(updatedMovieData)
+    setMovies(updatedMovieData)
+  }
 
   const filterMovieList = (str) => {
     let filteredMovies;
@@ -25,6 +34,7 @@ function MovieList() {
   return (
     <div>
       <FilterMovies filterMovies={filterMovieList} />
+      <AddMovie addMovieHandler={addMovie}/>
       {movies.map((movie) => {
         return <MovieCard key={movie._id} movie={movie} />;
       })}
