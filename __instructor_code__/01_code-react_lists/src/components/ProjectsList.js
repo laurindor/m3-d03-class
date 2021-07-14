e2748c5cd417a0fe7e0270d7ff0db2560726dec1
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-07-14 10:16:36 +0200

    WIP step 2 create JS from objects

---
+++ __instructor_code__/01_code-react_lists/src/components/ProjectsList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const projects = [
+    { _id: "127fae", name: "EatBCN", techStack: "Express, Node, React" },
+    { _id: "985afw", name: "TravelLite", techStack: "React, Express, Redux" },
+    { _id: "347eef", name: "IronClub", techStack: "Express, HBS, Mongo" },
+    { _id: "90af21", name: "ChatApp", techStack: "React, Express, SocketIO" },
+  ];
+
+  export default function ProjectsList(){
+      return(
+        <div className="list">
+        <h2>ProjectList</h2>
+        {projects.map((project) => {
+          return (
+            <div key={project._id}>
+              <h3>{project.name}</h3>
+              <p>Tech Stack: {project.techStack}</p>
+            </div>
+          )
+        })}
+      </div>
+      )
+  }
+
---
+++ __instructor_code__/01_code-react_lists/src/components/StudentsList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const studentsList = ['Morgane', 'Miki', 'Sebas', 'Raul','Lau', 'Victor', 'Danny']
+const JSXstudentsList = [<li>Morgane</li>, <li>Miki</li>, <li>Sebas</li>, <li>Raul</li>, <li>Lau</li>, <li>Victor</li>, <li>Danny</li>]
+
+const emptyArray = []
+
+export default function StudentsList(){
+return(
+<div className= 'list'>
+    <h2>List of PizzaBytes students</h2>
+    <ul>
+        {studentsList.map((student) => {
+            return <li>{student}</li>
+        })}
+    </ul>
+    <ul>
+        {JSXstudentsList}
+    </ul>
+</div>
+)
+}
