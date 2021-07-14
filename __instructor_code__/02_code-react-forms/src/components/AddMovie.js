5d9d6838df61588b96f0e45524b5d9fc637fd0b9
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-07-14 16:19:29 +0200

    fix AddMovie

--- __instructor_code__/02_code-react-forms/src/components/AddMovie.js
+++ __instructor_code__/02_code-react-forms/src/components/AddMovie.js
@@ -8,16 +8,16 @@ function AddMovie(props) {
       <h4>Add a Movie</h4>
       <form >
         <label>Title:</label>
-        <input type="text" name="title" value={title} />
+        <input type="text" name="title" />
 
         <label>Director:</label>
-        <input type="text" name="director" value={director} />
+        <input type="text" name="director" />
 
         <label>IMDB Rating:</label>
-        <input type="number" name="IMDBRating" value={IMDBRating} />
+        <input type="number" name="IMDBRating" />
 
         <label>Won Oscars:</label>
-        <input type="checkbox" name="hasOscars" checked={hasOscars} />
+        <input type="checkbox" name="hasOscars" />
         <button type="submit">Add a Movie</button>
       </form>
     </div>
